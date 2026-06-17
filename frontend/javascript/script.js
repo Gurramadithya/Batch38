eee24fbcf5810371656b03bb8b5dc9e488364ed0
Gurramadithya <gurramadithya45@gmail.com>
2026-06-17 23:39:58 +0530

    java script

---
+++ frontend/javascript/script.js
@@ -0,0 +1,33 @@
+var a = 10;
+var message = "Hello my self java script"
+var b = 10.1112;
+var condition = true;
+
+console.log(a, message, b, condition);
+console.log(typeof a, typeof message, typeof b, typeof condition);
+
+
+var c;
+console.log(c);
+console.log(typeof c);
+
+var d = null;
+console.log(d);
+console.log(typeof d);
+
+
+var e = 1 * "city"
+console.log(e);          //  not a number
+console.log(typeof e)
+
+
+var f = 1 + "hello";        //type conversion
+console.log(f); //string
+console.log(typeof f);
+
+var g = 1 + true;           //type conversion
+console.log(g); // number
+console.log(typeof g);
+
+
+var h=1
